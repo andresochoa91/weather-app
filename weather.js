fe8eddbe707cb01832cd0e97fd943c825fc14309
andresochoa91 <andres_ochoa91@hotmail.com>
2020-07-02 15:10:46 -0700

    Code ready to refactor

--- weather.js
+++ weather.js
@@ -1,4 +1,4 @@
-const apiKey = "115d1787d75817135c5ddd81a0a676f4";
+const openWeatherApiKey = "115d1787d75817135c5ddd81a0a676f4";
 let tbody =  document.querySelector("tbody");
 let info = document.querySelector("#info");
 let input = document.querySelector("#city");
@@ -7,10 +7,9 @@ let tempCity;
 let city;
 let country;
 
-
-let geoApiKey = "48a4a872ee984658a60d4d62f4740f1a";
   
 var map = L.map("map").setView([37.773972, 	-122.431297], 10);
+
 L.tileLayer("https://api.maptiler.com/maps/streets/256/{z}/{x}/{y}.png?key=cg31IX4S34d8DeEk9ob3", {
   attribution: `<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>`,
 }).addTo(map);
@@ -19,19 +18,15 @@ map.on('click', onMapClick);
 
 
 function onMapClick(event) {
-  console.log(event)
+  // console.log(event)
   let lat = event.latlng.lat;
   let lon = event.latlng.lng;
-  console.log(lat, lon)
-  fetch(`https://api.opencagedata.com/geocode/v1/json?q=${lat},${lon}&key=${geoApiKey}`)
+
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${openWeatherApiKey}`)
     .then(response => response.json())
     .then(data => {
-      console.log(data)
-      let city = data.results[0].components.city;
-      let country = data.results[0].components["ISO_3166-1_alpha-2"];
-      console.log(country)
-      
-      generateContent(city, country, lat, lon)
+      let city = data.name;
+      let country = data.sys.country;
       
       if (city) {
         var popup = L.popup()
@@ -39,8 +34,8 @@ function onMapClick(event) {
           .setContent(city)
           .openOn(map);
       }
-
-    })    
+      generateContent(city, country, lat, lon);
+    }) 
 }
 
 
@@ -48,21 +43,44 @@ input.addEventListener("keyup", (event) => {
   tempCity = event.target.value;
 });
 
-function weekTable (lat, lon) {
+function weekTable (lat, lon, currentWeather) {
   document.querySelector("table").classList.remove("d-none");
-  let getMinMaxWeather = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+  let getMinMaxWeather = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${openWeatherApiKey}&units=imperial`;
   fetch(getMinMaxWeather)
     .then(response => response.json())
     .then(data => {
-      console.log(data) /* I'M WORKING WITH THIS */
+      if (!currentWeather) {
+        document.querySelector("#current-weather").innerHTML = `
+          <span>Current Weather:</span> ${data.current.weather[0].description}
+        `;
+      }
+
       let days = "";
+      let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
       for (let i = 0; i < data.daily.length; i++) {
+        
+        
+        let currentDate = new Date();
+        currentDate.setDate(currentDate.getDate() + i);
+        
+        let dayOfWeek = (daysOfWeek[currentDate.getDay()]);
+
+        if (i === 0) {
+          currentDate = "Today";
+        } else if (i === 1) {
+          currentDate = "Tomorrow";
+        } else {
+          currentDate = currentDate.toLocaleDateString();         
+        }
+
         let min = data.daily[i].temp.min;
         let max = data.daily[i].temp.max;
         let icon = `http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png`;
         days += `
           <tr>
-            <th scope="row">${i}</th>
+            <th scope="row">${dayOfWeek}</th>
+            <th scope="row">${currentDate}</th>
             <td>${min}</td>
             <td>${max}</td>
             <td><img src="${icon}"></td>
@@ -70,17 +88,26 @@ function weekTable (lat, lon) {
         `;
       }
       tbody.innerHTML = days;
+      document.querySelector("#btn-modal").click();
     })
     .catch(err => console.log(err)); 
 }
 
 function generateContent (city, country, lat, lon, currentWeather) {
+  let cityTag = ""
+  let countryTag = ""
+  if (city) {
+    cityTag = `<p><span>City:</span> ${city}</p>`
+  }
+  if (country) {
+    countryTag = `<p><span>City:</span> ${country}</p>`
+  } 
   info.innerHTML = `
-    <p><span>City:</span> ${city}</p>
-    <p><span>Country:</span> ${country}</p>
-    <p><span>Current Weather:</span> ${currentWeather}</p>
+    ${cityTag}
+    ${countryTag}
+    <p id="current-weather"><span>Current Weather:</span> ${currentWeather}</p>
   `;
-  weekTable(lat, lon);
+  weekTable(lat, lon, currentWeather);
 } 
 
 function weatherInformation (cityWeather) {
@@ -89,6 +116,8 @@ function weatherInformation (cityWeather) {
     .then(data => {
       let lat = data.coord.lat;
       let lon = data.coord.lon;
+      map.setView(new L.LatLng(lat, lon), 11);
+
       city = data.name;
       country = data.sys.country;
       currentWeather = data.weather[0].description;
@@ -103,7 +132,13 @@ function weatherInformation (cityWeather) {
 }
 
 search.addEventListener("click", () => {
-  let getCityWeather = `https://api.openweathermap.org/data/2.5/weather?q=${tempCity}&appid=${apiKey}`;
+  let getCityWeather = `https://api.openweathermap.org/data/2.5/weather?q=${tempCity}&appid=${openWeatherApiKey}`;
   weatherInformation(getCityWeather);
+})
+
+input.addEventListener("keyup", (event) => {
+  if (event.keyCode === 13) {
+    search.click();
+  }
 })
 
